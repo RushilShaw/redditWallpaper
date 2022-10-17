d298880bd921b734309504296ba84212afa931ba
RushilShaw <56496445+RushilShaw@users.noreply.github.com>
2022-10-16 20:37:33 -0400

    comments and improving functions

---
+++ README.md
@@ -0,0 +1,19 @@
+# Reddit Wallpaper
+
+This project when run will take a wallpaper from reddit. The subreddit specified from the subreddit global variable adjusted to the resolution of your monitor.
+
+# Requirements
+
+Packages specified in requirements.txt
+
+Made using python 3.7 - should work for any 3.5+ Version though
+
+An internet connection
+
+a .env file including
+
+```python
+reddit_client_id='Your Client ID'
+reddit_client_secret='Your Client Secreat'
+reddit_user_agent='Your User Agent Identifier'
+```
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-import praw
-import requests
-import ctypes
-import os
-from PIL import Image
 import re
-from dotenv import dotenv_values
+import os
+import praw
+import ctypes
 import random
+import requests
+from PIL import Image
+from dotenv import dotenv_values
 
 
 # Modifiable Globals
 subreddit = 'wallpaper'
 image_file_path = './wallpaper.jpg'
 generator_limit = 100
+time_filter = "year"  # can be hour, day, week, year, or all
 
 # Unmodifiable Globals
 jpg_or_png = re.compile(r"\.(?:jpg|png)$")
@@ -22,13 +23,15 @@ resolution = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
 absolute_path = os.path.abspath(image_file_path)
 
 
-def download_image(image_url):
+def download_image(image_url, path):
+    """downloads an image from the internet and puts in into the specified path"""
     img_data = requests.get(image_url).content
-    with open(image_file_path, 'wb') as handler:
+    with open(path, 'wb') as handler:
         handler.write(img_data)
 
 
 def change_resolution(new_resolution, image_path):
+    """take an image given its path and changes it to the new specified resolution"""
     (
         Image.open(image_path)
         .convert('RGB')
@@ -44,13 +47,13 @@ def main():
         user_agent=config.get('reddit_user_agent')
     )
     submission_list = []
-    submission_generator = reddit.subreddit(subreddit).top(time_filter="week", limit=generator_limit)
+    submission_generator = reddit.subreddit(subreddit).top(time_filter=time_filter, limit=generator_limit)
     for submission in submission_generator:
         if jpg_or_png.findall(submission.url):
             submission_list.append(submission)
     submission = random.choice(submission_list)
     url = submission.url
-    download_image(url)
+    download_image(url, image_file_path)
     change_resolution(resolution, image_file_path)
     ctypes.windll.user32.SystemParametersInfoW(20, 0, absolute_path, 0)  # changes the window's backdrop
 
