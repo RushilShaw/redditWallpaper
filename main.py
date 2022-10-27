d0d017d94fbc11324501f13b90abdc686ddc6ee9
RushilShaw <56496445+RushilShaw@users.noreply.github.com>
2022-10-27 18:11:26 -0400

    added flair support and removed all global variables

--- main.py
+++ main.py
@@ -8,21 +8,6 @@ from PIL import Image
 from dotenv import dotenv_values
 
 
-# Modifiable Globals
-subreddit = 'wallpaper'
-image_file_path = './wallpaper.jpg'
-generator_limit = 100
-time_filter = "year"  # can be hour, day, week, year, or all
-
-# Unmodifiable Globals
-jpg_or_png = re.compile(r"\.(?:jpg|png)$")
-config = dotenv_values(".env")
-user32 = ctypes.windll.user32
-user32.SetProcessDPIAware()
-resolution = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
-absolute_path = os.path.abspath(image_file_path)
-
-
 def download_image(image_url, path):
     """downloads an image from the internet and puts in into the specified path"""
     img_data = requests.get(image_url).content
@@ -34,28 +19,56 @@ def change_resolution(new_resolution, image_path):
     """take an image given its path and changes it to the new specified resolution"""
     (
         Image.open(image_path)
-        .convert('RGB')
-        .resize(new_resolution)
-        .save(image_path)
+             .convert('RGB')
+             .resize(new_resolution)
+             .save(image_path)
     )
 
 
-def main():
-    reddit = praw.Reddit(
-        client_id=config.get('reddit_client_id'),
-        client_secret=config.get('reddit_client_secret'),
-        user_agent=config.get('reddit_user_agent')
-    )
+def download_reddit_image(reddit_instance: praw.Reddit, image_path, subreddit, flair, limit, time_filter):
+    """downloads a random image from reddit"""
+    jpg_or_png = re.compile(r"\.(?:jpg|png)$")
     submission_list = []
-    submission_generator = reddit.subreddit(subreddit).top(time_filter=time_filter, limit=generator_limit)
+    if flair == '':
+        submission_generator = reddit_instance.subreddit(subreddit).top(time_filter=time_filter, limit=limit)
+    else:
+        submission_generator = reddit_instance.subreddit(subreddit).search(
+            f'flair:"{flair}"',
+            time_filter=time_filter,
+            limit=limit
+        )
     for submission in submission_generator:
         if jpg_or_png.findall(submission.url):
             submission_list.append(submission)
     submission = random.choice(submission_list)
     url = submission.url
-    download_image(url, image_file_path)
-    change_resolution(resolution, image_file_path)
+    download_image(url, image_path)
+
+
+def main():
+    config = dotenv_values(".env")
+    image_file_path = './wallpaper.jpg'
+    absolute_path = os.path.abspath(image_file_path)
+    user32 = ctypes.windll.user32
+    user32.SetProcessDPIAware()
+    resolution = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
     ctypes.windll.user32.SystemParametersInfoW(20, 0, absolute_path, 0)  # changes the window's backdrop
+    my_reddit = praw.Reddit(
+        client_id=config.get('reddit_client_id'),
+        client_secret=config.get('reddit_client_secret'),
+        user_agent=config.get('reddit_user_agent'),
+        username=config.get('reddit_username'),
+        password=config.get('reddit_password')
+    )
+    download_reddit_image(
+        reddit_instance=my_reddit,
+        image_path=image_file_path,
+        subreddit=config['subreddit'],
+        flair=config['flair'],
+        limit=int(config['limit']),
+        time_filter=config['time_filter']
+    )
+    change_resolution(resolution, image_file_path)
 
 
 if __name__ == '__main__':
